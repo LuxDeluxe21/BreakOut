db13f2f5ab95493db55d71a610ef2c105a3d240b
LuxDeluxe21 <lucianomoya1995@gmail.com>
2025-06-19 19:48:47 -0400

    Tarea Módulo 14
    Creé dos niveles duplicados del nivel original
    Creé dos scripteable objects para el putnaje alto de cada nuevo nivel
    Cambié la resistencia de los bloques en cada nivel

--- Breakout/Assets/_Srcipts/Bola.cs
+++ Breakout/Assets/_Srcipts/Bola.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Bola : MonoBehaviour
 {
     public bool isGameStarted = false;
     [SerializeField] public float velocidadBola = 10.0f;
+    Vector3 ultimaPosicion = Vector3.zero;
+    Vector3 direccion = Vector3.zero;
+    Rigidbody rigidbody;
+    private ControlBordes control;
+    public UnityEvent BolaDestruida;
+
+    private void Awake()
+    {
+        control = GetComponent<ControlBordes>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        isGameStarted = false;
         Vector3 PosicionInicial = GameObject.FindGameObjectWithTag("Jugador").transform.position;
         PosicionInicial.y += 3;
         this.transform.position = PosicionInicial;
         this.transform.SetParent(GameObject.FindGameObjectWithTag("Jugador").transform);
+        rigidbody = this.gameObject.GetComponent<Rigidbody>();
 
 
     }
@@ -20,6 +34,46 @@ public class Bola : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (control.salioAbajo)
+        {
+            BolaDestruida.Invoke();
+            Destroy(this.gameObject);
+        }
+        if (control.salioArriba)
+        {
+            direccion = transform.position - ultimaPosicion;
+            Debug.Log("La bola toco el borde superior");
+            direccion.y *= -1;
+            direccion = direccion.normalized;
+            rigidbody.velocity = velocidadBola * direccion;
+            control.salioArriba = false;
+            control.enabled = false;
+            Invoke("HabilitarControl", 0.5f);
+        }
+        if (control.salioDerecha)
+        {
+            direccion = transform.position - ultimaPosicion;
+            Debug.Log("La bola toco el borde derecho");
+            direccion.x *= -1;
+            direccion = direccion.normalized;
+            rigidbody.velocity = velocidadBola * direccion;
+            control.salioDerecha = false;
+            control.enabled = false;
+            Invoke("HabilitarControl", 0.5f);
+        }
+        if (control.salioIzquierda)
+        {
+            direccion = transform.position - ultimaPosicion;
+            Debug.Log("La bola toco el borde izquierdo");
+            direccion.x *= -1;
+            direccion = direccion.normalized;
+            rigidbody.velocity = velocidadBola * direccion;
+            control.salioIzquierda = false;
+            control.enabled = false;
+            Invoke("HabilitarControl", 0.5f);
+        }
+
+
         if (Input.GetKey(KeyCode.Space)|| Input.GetButton("Submit"))
         {
             if (!isGameStarted)
@@ -29,5 +83,18 @@ public class Bola : MonoBehaviour
                 GetComponent<Rigidbody>().velocity = velocidadBola * Vector3.up;
             }
         }
+    }
+
+    private void HabilitarControl()
+    {
+        control.enabled = true;
+    }
+    private void FixedUpdate()
+    {
+        ultimaPosicion = transform.position;
+    }
+    public void LateUpdate()
+    {
+        if (direccion != Vector3.zero) direccion = Vector3.zero;
     }
 }
